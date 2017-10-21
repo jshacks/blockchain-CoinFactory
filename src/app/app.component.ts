d41b712fbf74a6b13e16f42136246b1b63d89acf
Andrei <andressh11@gmail.com>
2017-10-21 14:32:45 +0300

    added icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  imgUrl = '../assets/img/coin-factory.svg'
+
+  ngOnInit(){
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '../materialmodule/material.module';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { IconSvgExample } from '../iconcomponent/icon.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IconSvgExample
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
